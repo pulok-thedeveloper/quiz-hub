32543b05e0145281bf1492eed2bc8884087134f9
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-10-13 15:30:38 +0600

    Options Added

---
+++ src/Componenets/Option/Option.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Option.css';
+
+import { ToastContainer } from 'react-toastify';
+
+const Option = ({option,name,answerStatus}) => {
+    return (
+        <div className='my-3  p-3 rounded option'>
+            <input type="radio" name={name} id={option} />
+            <label onClick={()=>answerStatus({option})} className='ml-3 hover:bg-slate-500 p-3 rounded' htmlFor={option}>{option}</label>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Option;
