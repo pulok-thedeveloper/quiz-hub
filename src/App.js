58691c31d3157c1d9dd5f80287fa3e80238c01ae
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-10-13 15:19:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Componenets/Home/Home';
+import Questions from './Componenets/Questions/Questions';
+import Stats from './Componenets/Stats/Stats';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', 
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path: '/:id',
+          loader: async({params})=>{
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+          },
+          element: <Questions></Questions>
+        },
+        {
+          path: '/stats',
+          element: <Stats></Stats>
+        }
+      ],
+      element: <Main></Main>
+      
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
